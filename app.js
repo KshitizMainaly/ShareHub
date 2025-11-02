ced31b77a4728dc4ec6b77cf7e7b5fbd86fd45b3
kshitiz <kshitizmainaly3334@gmail.com>
2025-11-02 17:39:58 +0545

    feat(backend):add ability to render ejs and render header ejs on '/' route

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const PORT = 4000;
 const app = express();
-app.get("/", (req, res) => {
-  res.json({ message: "you are home " });
-});
 
+//middleware start
+
+app.set("view engine", "ejs");
+//middleware end
+
+app.get("/", (req, res) => {
+  res.render("header");
+});
 app.listen(process.env.PORT || PORT, () =>
-  console.log(`application is listening at port ${PORT}`),
+  console.log(`application is listening at port ${process.env.PORT}`),
 );
